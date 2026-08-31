418a95f599e13599f999fb2cf5322a37a01eac6f
dasiuss <dasiuss@gmail.com>
2026-08-31 14:37:32 +0200

    Throttle BLE notifications to protect NimBLE buffers

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ble-test-console-v14";
+const CACHE_NAME = "ble-test-console-v15";
 const APP_ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
